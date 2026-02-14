598463196d081b6d7f6258f04da4d8aebdd301c5
futuroelectra <connorjoejoseph@gmail.com>
2026-02-14 10:19:04 +0200

    Add GitHub Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/toyboy/", // required for GitHub Pages (https://futuroelectra.github.io/toyboy/)
   plugins: [react(), tailwindcss()],
 });
